2fdf84dc1932328316bb2d2bc562fd6cfe5c6533
Joshua Sullivan <jmsulli3@uci.edu>
2025-01-08 20:32:52 -0800

    Finished tokenzier, more testing to be done

---
+++ PartA.py
@@ -0,0 +1,26 @@
+def tokenize(filepath: str) -> list[str]:
+    tokens: list[str] = list()
+    current_token = ''
+
+    # Read parameter filepath
+    with open(filepath, 'r', encoding='utf-8') as file:
+        # Iterate through file character-by-character until EOF
+        while char := file.read(1).lower():
+            # Not whitespace == add char to current token, else add to token list
+            if not char.isspace():
+                current_token += char
+            else:
+                if current_token:
+                    tokens.append(current_token)
+                    current_token = ''
+                    
+        # Adds token at EOF if its truthy
+        if current_token: 
+            tokens.append(current_token) 
+
+    return tokens
+            
+
+if __name__ == '__main__':
+    filepath: str = input("Please enter a file path to read: ")
+    print(tokens := tokenize(filepath))
